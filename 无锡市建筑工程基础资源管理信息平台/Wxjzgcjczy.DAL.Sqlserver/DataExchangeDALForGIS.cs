39c2f295fb2afed41cccc976959a7d931b618b1c
39189058@qq.com <39189058@qq.com>
2017-12-06 21:43:06 +0800

    按成工的要求更新

--- 无锡市建筑工程基础资源管理信息平台/Wxjzgcjczy.DAL.Sqlserver/DataExchangeDALForGIS.cs
+++ 无锡市建筑工程基础资源管理信息平台/Wxjzgcjczy.DAL.Sqlserver/DataExchangeDALForGIS.cs
@@ -19,8 +19,9 @@ namespace Wxjzgcjczy.DAL.Sqlserver
     {
         public DBOperator DB { get; set; }
 
-        public static string PROJECTINFO_FIELDS = @"a.PrjNum,a.PrjName,a.PrjTypeNum,a.BuildCorpName,a.BuildCorpCode,a.ProvinceNum,"
-            +"a.CityNum,a.CountyNum,a.BDate,a.EDate,a.jd,a.wd,b.programme_address,a.DocNum,a.DocCount";
+        public static string PROJECTINFO_FIELDS = @"a.PrjNum,a.PrjName,a.PrjTypeNum,a.BuildCorpCode,a.BuildCorpName,"
+            + "a.EconCorpName,a.DesignCorpName,a.ConsCorpName,a.SuperCorpName,a.CensorCorpName,"
+            + "a.ProvinceNum,a.CityNum,a.CountyNum,a.BDate,a.EDate,ISNULL(a.jd1, a.jd) jd, ISNULL(a.wd1, a.wd) wd,b.programme_address,a.DocNum,a.DocCount,a.EDates";
      
 
         #region 获取项目数据
@@ -115,50 +116,14 @@ namespace Wxjzgcjczy.DAL.Sqlserver
         /// <summary>
         /// 获取项目
         /// </summary>
-        /// <param name="range">经纬度范围</range>
+        /// <param name="range">经纬度范围</param>
+        /// <param name="qy">区域代码</param>
+        /// <param name="beginDate">开始日期</param>
+        /// <param name="endDate">截止日期</param>
         /// <returns></returns>
-        public DataTable GetProjectByRange(string range)
+        public DataTable GetProjectByRange(string range, string qy, string beginDate, string endDate)
         {
             SqlParameterCollection sp = this.DB.CreateSqlParameterCollection();
-
-            //parse range
-            double jd1, jd2, wd1, wd2;
-
-            int indexOfSep = range.IndexOf(";");
-            string part1 = range.Substring(0, indexOfSep);
-            string part2 = range.Substring(indexOfSep + 1);
-
-            indexOfSep = part1.IndexOf(":");
-            string part11 = part1.Substring(0, indexOfSep);
-            string part12 = part1.Substring(indexOfSep + 1);
-
-            indexOfSep = part2.IndexOf(":");
-            string part21 = part2.Substring(0, indexOfSep);
-            string part22 = part2.Substring(indexOfSep + 1);
-
-            if (double.Parse(part11) < double.Parse(part21))
-            {
-                jd1 = double.Parse(part11);
-                jd2 = double.Parse(part21);
-            }
-            else
-            {
-                jd2 = double.Parse(part11);
-                jd1 = double.Parse(part21);
-            }
-
-            if (double.Parse(part12) < double.Parse(part22))
-            {
-                wd1 = double.Parse(part12);
-                wd2 = double.Parse(part22);
-            }
-            else
-            {
-                wd2 = double.Parse(part12);
-                wd1 = double.Parse(part22);
-            }
-
-            //parse range finish
 
             StringBuilder sb = new StringBuilder();
             sb.Append(" select ");
@@ -167,18 +132,77 @@ namespace Wxjzgcjczy.DAL.Sqlserver
             sb.Append(" left join TBProjectAdditionalInfo b on b.PrjNum=a.PrjNum");
             sb.Append(" where 1=1");
 
-            sp.Add("@jd1", jd1);
-            sb.Append(" and a.jd>=@jd1");
+            if(!string.IsNullOrEmpty(range))
+            {
+                //parse range
+                double jd1, jd2, wd1, wd2;
 
-            sp.Add("@jd2", jd2);
-            sb.Append(" and a.jd<=@jd2");
+                int indexOfSep = range.IndexOf(";");
+                string part1 = range.Substring(0, indexOfSep);
+                string part2 = range.Substring(indexOfSep + 1);
 
-            sp.Add("@wd1", wd1);
-            sb.Append(" and a.wd>=@wd1");
+                indexOfSep = part1.IndexOf(":");
+                string part11 = part1.Substring(0, indexOfSep);
+                string part12 = part1.Substring(indexOfSep + 1);
 
-            sp.Add("@wd2", wd2);
-            sb.Append(" and a.wd<=@wd2");
+                indexOfSep = part2.IndexOf(":");
+                string part21 = part2.Substring(0, indexOfSep);
+                string part22 = part2.Substring(indexOfSep + 1);
 
+                if (double.Parse(part11) < double.Parse(part21))
+                {
+                    jd1 = double.Parse(part11);
+                    jd2 = double.Parse(part21);
+                }
+                else
+                {
+                    jd2 = double.Parse(part11);
+                    jd1 = double.Parse(part21);
+                }
+
+                if (double.Parse(part12) < double.Parse(part22))
+                {
+                    wd1 = double.Parse(part12);
+                    wd2 = double.Parse(part22);
+                }
+                else
+                {
+                    wd2 = double.Parse(part12);
+                    wd1 = double.Parse(part22);
+                }
+
+                //parse range finish
+
+                sp.Add("@jd1", jd1);
+                sb.Append(" and ISNULL(a.jd1, a.jd)>=@jd1");
+
+                sp.Add("@jd2", jd2);
+                sb.Append(" and ISNULL(a.jd1, a.jd)<=@jd2");
+
+                sp.Add("@wd1", wd1);
+                sb.Append(" and ISNULL(a.wd1, a.wd)>=@wd1");
+
+                sp.Add("@wd2", wd2);
+                sb.Append(" and ISNULL(a.wd1, a.wd)<=@wd2");
+            }
+
+            if (!string.IsNullOrEmpty(qy))
+            {
+                sp.Add("@qy", qy);
+                sb.Append(" and a.CountyNum=@qy");
+            }
+
+            if (!string.IsNullOrEmpty(beginDate))
+            {
+                sp.Add("@beginDate", beginDate);
+                sb.Append(" and SUBSTRING(convert(VARCHAR(30), a.xgrqsj, 120), 1, 10)>=@beginDate");
+            }
+
+            if (!string.IsNullOrEmpty(endDate))
+            {
+                sp.Add("@endDate", endDate);
+                sb.Append(" and SUBSTRING(convert(VARCHAR(30), a.xgrqsj, 120), 1, 10)<=@endDate");
+            }
 
             return DB.ExeSqlForDataTable(sb.ToString(), sp, "TBProjectInfo");
 
