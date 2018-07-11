2423309e642dd775322ad0768d53a06b034d3b6d
39189058@qq.com <39189058@qq.com>
2018-07-11 09:15:50 +0800

    update dbscript for docnumfrom/docnumto/doccount in xm_gcdjb_dtxm_doc

--- db/db_upgrade_4gis_20180709.sql
+++ db/db_upgrade_4gis_20180709.sql
@@ -1,3 +1,3 @@
 alter table [dbo].[xm_gcdjb_dtxm_doc] add [docnumfrom] varchar(100) null;
 alter table [dbo].[xm_gcdjb_dtxm_doc] add [docnumto] varchar(100) null;
-alter table [dbo].[xm_gcdjb_dtxm_doc] add [doccount] varchar(100) null;
+alter table [dbo].[xm_gcdjb_dtxm_doc] add [doccount] [int] null;
