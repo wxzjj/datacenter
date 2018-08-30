068e49d595c48c07232e7712fc492de5e29c0957
glq0316 <39189058@qq.com>
2018-08-30 14:54:25 +0800

    update table [xzcf]

---
+++ db/db_upgrade_20180830.sql
@@ -0,0 +1,8 @@
+﻿USE [WJSJZX]
+GO
+
+/* 增加来源，更新时间 */
+alter table [dbo].[xzcf] add source [varchar](30) NULL;
+alter table [dbo].[xzcf] add updateDate [datetime] NULL;
+alter table [dbo].[xzcf] alter column prjNum [varchar](30) NULL;
+
